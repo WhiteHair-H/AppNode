c56e31a4423f7179cfbf7b500b1b3c268f8ca345
jinwooha <pddana@naver.com>
2022-06-27 14:25:46 +0900

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AppNode
+
+## 참조
+1.
+https://devdhjo.github.io/mysql/2020/01/28/database-mysql-001.html
+
+2.
+https://1mini2.tistory.com/88
+
+3.
+https://poiemaweb.com/nodejs-mysql
--- mysql.js
+++ mysql.js
@@ -1,16 +1,16 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  user     : 'TESTUSER',
+  password : '1234',
+  database : 'testdb'
 });
- 
+
 connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+
+connection.query('SELECT * from Users', (error, rows, fields) => {
   if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+  console.log('User info is: ', rows);
 });
- 
+
 connection.end();
